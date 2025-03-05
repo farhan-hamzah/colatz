b7fe70b3e6e0883b81ce86a21df2725f912a675d
Farhan Hamzah <177802321+farhan-hamzah@users.noreply.github.com>
2025-03-05 10:59:12 +0700

    Add files via upload

---
+++ latihan.go
@@ -0,0 +1,17 @@
+// colatz
+// package main
+// import "fmt"
+
+// func main(){
+//     var masukan int
+//     fmt.Scan(&masukan)
+//     for masukan != 1{
+//         if masukan%2 == 0{
+//             masukan= masukan/2
+//             fmt.Println(masukan)
+//         }else{
+//             masukan = 3*masukan+1
+//             fmt.Println(masukan)
+//         }
+//     }
+// }
